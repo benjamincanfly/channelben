296b1d34b34433579d90281c86a6dca16acd90fc
Benjamin <benjamincanfly@gmail.com>
2014-07-29 13:04:58 -0400

    OG stuff, image

--- index.php
+++ index.php
@@ -19,29 +19,39 @@ error_reporting(0);
 
 	  ga('create', 'UA-1029946-17', 'auto');
 	  ga('send', 'pageview');
-
+	  
 	</script>
 	
 	<base target="_blank">
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Channel Ben</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="description" content="Welcome, idiot! My name is Benjamin Apple and I post a original, funny YouTube videos every single weekday. Watch and subscribe!">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta property="og:title" content="Channel Ben" />
+	<meta property="og:type" content="website" />
+	<meta property="og:description" content="Welcome, idiot! My name is Benjamin Apple and I post a original, funny YouTube videos every single weekday. Watch and subscribe!" />
+	<meta property="og:url" content="http://www.channelben.com" />
+	
+	<meta property="og:image" content="http://www.channelben.com/img/channel_ben_og.jpg">
+	<meta property="og:image:type" content="image/jpeg">
+	<meta property="og:image:width" content="1920">
+	<meta property="og:image:height" content="1080">
+	
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-
-		<link rel="alternate" type="application/rss+xml" title="RSS" href="http://gdata.youtube.com/feeds/base/users/BenjaminApple/uploads?alt=rss&amp;v=2&amp;orderby=published&amp;client=ytapi-youtube-profile"/>
-		
-		<link rel="shortcut icon" type="image/png" href="/img/favicon.png"/>
-		<link rel="icon" type="image/png" href="/img/favicon.png"/>
-		
+	
+	<link rel="alternate" type="application/rss+xml" title="RSS" href="http://gdata.youtube.com/feeds/base/users/BenjaminApple/uploads?alt=rss&amp;v=2&amp;orderby=published&amp;client=ytapi-youtube-profile"/>
+	
+	<link rel="shortcut icon" type="image/png" href="/img/favicon.png"/>
+	<link rel="icon" type="image/png" href="/img/favicon.png"/>
+	
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
         <link rel="stylesheet" href="css/style.css">
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
     </head>
     <body>
+		
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
@@ -50,7 +60,7 @@ error_reporting(0);
 
 		<div id="everything">
 			<a href="http://www.channelben.com" id="logo"><img src="/img/channelben.png"/></a>
-			<div id="header">Hey idiot! I'm <a href='http://www.twitter.com/benjaminapple'>Benjamin Apple</a> and I upload a video to my <a href="http://www.youtube.com/benjaminapple">YouTube channel</a> Monday through Friday every single week. <a href="mailto:benjamincanfly@gmaiml.com">Email me!</a></div>
+			<div id="header">Welcome, idiot! I'm <a href='http://www.twitter.com/benjaminapple'>Benjamin Apple</a> and I post original, funny videos on my <a href="http://www.youtube.com/benjaminapple">YouTube channel</a> Monday through Friday every single week. <a href="mailto:benjamincanfly@gmaiml.com">Email me!</a></div>
 			
 			<div id="content">
 					<div id="text">
